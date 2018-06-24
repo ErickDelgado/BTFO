9343481724a825ce6c4a6bb2b97dadab178c1500
Erick Delgado <erickdelgado@protonmail.com>
2018-06-23 23:06:17 -0400

    crypto data sanitized
    historical data garnered from ten coins and stored in store object

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BTFO
+
+Performance index which scrapes the historical data for the last four years of BTC, ETH, MANA, WAVES, BCC, Monero, Nuls, Polymath, NEO, and Binance Coin.
+
+Inflation adjustment, sharp ratio, mean price, volatility, liquidity measurements, default net asset value.
--- index.js
+++ index.js
@@ -2,25 +2,84 @@
   const request = require('request');
   let re = /(se).*?(,")/g;
   let store = {};
-   request('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
     let parse = response.body.match(re);
     for(i=0; i<parse.length; i++) {
-     parse[i] = parseInt(parse[i].replace(/\D/g,''))
+     parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
     };
     store.BTC = parse;
-});
-   request('https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-   let parse = response.body.match(re);
-   for(i=0; i<parse.length; i++) {
-    parse[i] = parseInt(parse[i].replace(/\D/g,''))
-   };
-   store.ETH = parse;
   });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.ETH = parse;
+  });
+
   request('https://min-api.cryptocompare.com/data/histoday?fsym=MANA&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
   let parse = response.body.match(re);
   for(i=0; i<parse.length; i++) {
-   parse[i] = parseInt(parse[i].replace(/\D/g,''))
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
   };
-  store.ETH = parse;
- });
+  store.MANA = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=XMR&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.XMR = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=POLY&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.POLY = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=NULS&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.NULS = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=NEO&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.NEO = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=BCH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.BCH = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=BNB&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.BNB = parse;
+  });
+
+  request('https://min-api.cryptocompare.com/data/histoday?fsym=WAVES&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = response.body.match(re);
+  for(i=0; i<parse.length; i++) {
+   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+  };
+  store.WAVES = parse;
+  console.log(store.WAVES)
+  });
 })();
