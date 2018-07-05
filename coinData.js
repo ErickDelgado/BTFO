0e5d1e9363f6707a5b1a5871c5e830ae13fa0f79
ead139 <erickdelgado@protonmail.com>
2018-07-05 13:53:43 -0400

    updated api call
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- coinData.js
+++ coinData.js
@@ -1,135 +1,165 @@
 const request = require('request');
 
-let re = /(se).*?(,")/g;
+const re = /(se).*?(,")/g;
 let store = {};
-request('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-  let parse = response.body.match(re);
-  for(i=0; i<parse.length; i++) {
-   parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+request('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&allData=true&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
   };
-  store.BTC = parse;
+  store.BTC = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
+request('https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=USD&allData=true&aggregate=3&e=CCCAGG', function (error, response, body) {
+let parse = JSON.parse(response.body);
+console.log(parse.Data)
+let data = [];
+for(i=0; i<parse.Data.length; i++) {
+  data.push(parse.Data[i].close)
 };
-store.ETH = parse;
+  store.ETH = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=MANA&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.MANA = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=MANA&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.MANA = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=XMR&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.XMR = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=XMR&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.XMR = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=POLY&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.POLY = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=POLY&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.POLY = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=NULS&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.NULS = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=NULS&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.NULS = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=NEO&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.NEO = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=NEO&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.NEO = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=BCH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.BCH = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=BCH&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.BCH = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=BNB&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.BNB = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=BNB&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.BNB = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=WAVES&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.WAVES = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=WAVES&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.WAVES = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=XRP&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.XRP = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=XRP&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.XRP = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=XLM&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.XLM = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=XLM&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.XLM = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=DASH&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.DASH = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=DASH&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.DASH = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=ZEC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.ZEC = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=ZEC&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.ZEC = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=ETC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.ETC = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=ETC&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.ETC = data;
 });
 
-request('https://min-api.cryptocompare.com/data/histoday?fsym=LTC&tsym=USD&limit=2000&aggregate=3&e=CCCAGG', function (error, response, body) {
-let parse = response.body.match(re);
-for(i=0; i<parse.length; i++) {
- parse[i] = parseInt(parse[i].replace(/[^0-9.]/g,''));
-};
-store.LTC = parse;
+request('https://min-api.cryptocompare.com/data/histoday?fsym=LTC&tsym=USD&limit=550&aggregate=3&e=CCCAGG', function (error, response, body) {
+  let parse = JSON.parse(response.body);
+  console.log(parse.Data)
+  let data = [];
+  for(i=0; i<parse.Data.length; i++) {
+    data.push(parse.Data[i].close)
+  };
+  store.LTC = data;
 });
-
-module.exports = store
 
 module.exports = store;
