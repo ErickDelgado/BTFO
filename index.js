dbe3e49f7cf716b52068440f86d10deed5992b45
ead139 <erickdelgado@protonmail.com>
2018-07-10 13:33:13 -0400

    aggregating data
    Co-Authored-By: Erick Delgado <erickdelgado@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const btc = require('./data/btc.json');
+const bch = require('./data/bch.json');
+const bnb = require('./data/bnb.json');
+const dash = require('./data/dash.json');
+const etc = require('./data/etc.json');
+const eth = require('./data/eth.json');
+const ltc = require('./data/ltc.json');
+const mana = require('./data/mana.json');
+const neo = require('./data/neo.json');
+const nuls = require('./data/nuls.json');
+const poly = require('./data/poly.json');
+const waves = require('./data/waves.json');
+const xlm = require('./data/xlm.json');
+const xmr = require('./data/xmr.json');
+const xrp = require('./data/xrp.json');
+const zec = require('./data/zec.json');
 
+const data = Object.assign(btc, bch, bnb, dash, etc, eth, ltc, mana, neo, nuls, poly, waves, xlm, xmr, xrp, zec);
+console.log(data)
+
+module.exports = data;
